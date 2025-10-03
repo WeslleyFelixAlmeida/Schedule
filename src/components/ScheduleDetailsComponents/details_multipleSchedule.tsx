262f633aa9e351f821c7a0169b530ef0268616cc
ZunoGnG <weslleyfelixdealmeida@gmail.com>
2025-10-03 19:49:02 -0300

    Criando cards dos dias do mês apresentado e criando componentes para cada linha dos múltiplos status possíveis

--- src/components/ScheduleDetailsComponents/details_multipleSchedule.tsx
+++ src/components/ScheduleDetailsComponents/details_multipleSchedule.tsx
@@ -3,13 +3,69 @@ import ScheduleLine_choosed from "./MultipleSchedule_lines/ScheduleLine_choosed"
 import ScheduleLine_occupied from "./MultipleSchedule_lines/ScheduleLine_occupied";
 import ScheduleLine_open from "./MultipleSchedule_lines/ScheduleLine_open";
 
+import data from "./dados.json"; // Apagar depois, serve apenas para puxar dados ficticios.
+
 type Details_MultipleScheduleProps = {
     scheduleId: number;
 }
 
-const Details_MultipleSchedule = (props: Details_MultipleScheduleProps)=> {
+type scheduleLine_props = {
+    hour: string;
+    buttonFunction: Function;
+    keyID: number;
+}
 
-    const teste = ()=>{
+const scheduleLine_open_element = (props: scheduleLine_props) => {
+    return <ScheduleLine_open hour={props.hour} buttonFunction={props.buttonFunction} key={props.keyID} />
+}
+
+const scheduleLine_occupied_element = (props: Pick<scheduleLine_props, "hour" | "keyID">) => {
+    return <ScheduleLine_occupied hour={props.hour} key={props.keyID} />
+}
+
+const scheduleLine_choosed_element = (props: scheduleLine_props) => {
+    return <ScheduleLine_choosed hour={props.hour} buttonFunction={props.buttonFunction} key={props.keyID} />
+}
+
+
+const mostrarCards = () => {
+    let lines: any = []
+    lines.push(
+        scheduleLine_open_element(
+            {
+                buttonFunction: () => { console.log("Testando!") },
+                hour: "10:15",
+                keyID: 1
+            }
+        )
+    );
+
+    lines.push(
+
+        scheduleLine_occupied_element(
+            {
+                hour: "10:30",
+                keyID: 2
+            }
+        )
+    );
+
+    lines.push(
+        scheduleLine_choosed_element(
+            {
+                buttonFunction: () => { console.log("Testando!") },
+                hour: "10:45",
+                keyID: 3
+            }
+        )
+    );
+
+    return lines;
+}
+
+const Details_MultipleSchedule = (props: Details_MultipleScheduleProps) => {
+
+    const teste = () => {
         console.log("Teste")
     }
 
@@ -18,20 +74,24 @@ const Details_MultipleSchedule = (props: Details_MultipleScheduleProps)=> {
             <div className={style.containerList}>
                 <div className={style.topContainerList}>
                     <p>x</p>
-                    <h2>Datas disponíveis:</h2>
+                    <h2>Datas disponíveis - Outubro:</h2>
                 </div>
                 <div className={style.containerDate}>
                     <ul>
                         {Array.from({ length: 25 }, (_, i) => (//Apagar depois
-                            <li key={i}>Datas</li>
+                            <li key={i}>
+                                <div></div>
+                                <p>{i + 1}</p>
+                            </li>
                         ))}
                     </ul>
                 </div>
                 <h2>Horários disponíveis:</h2>
                 <ul>
-                    <ScheduleLine_open hour={"10:00"} buttonFunction={teste} key={1}/>
-                    <ScheduleLine_occupied hour={"10:30"} key={2}/>
-                    <ScheduleLine_choosed hour={"12:45"} buttonFunction={teste} key={3}/>
+                    {/* <ScheduleLine_open hour={"10:00"} buttonFunction={teste} key={1} />
+                    <ScheduleLine_occupied hour={"10:30"} key={2} />
+                    <ScheduleLine_choosed hour={"12:45"} buttonFunction={teste} key={3} /> */}
+                    {mostrarCards()}
                 </ul>
             </div>
         </div>
