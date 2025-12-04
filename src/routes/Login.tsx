9a158266f4a1b59d8516c3588711979b99099eda
ZunoGnG <weslleyfelixdealmeida@gmail.com>
2025-12-03 21:52:25 -0300

    Integrando front-end (Login e cadastro) com a API

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,13 +1,15 @@
 import { useForm } from "react-hook-form";
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useSearchParams } from "react-router-dom";
 import Logo from "../components/Logo";
 import style from "./Login.module.css";
+import { API_URL } from "../config";
+import { useEffect } from "react";
 
 const loginFormSchema = z.object({
-    email: z.email({ message: "Informe um e-mail válido" }),
-    userPassword: z.string().min(1, "O campo de senha está vazio"),
+    email: z.string().email({ message: "Informe um e-mail válido" }),
+    password: z.string().min(1, "O campo de senha está vazio"),
 });
 
 type LoginFormSchema = z.infer<typeof loginFormSchema>;
@@ -15,15 +17,46 @@ type LoginFormSchema = z.infer<typeof loginFormSchema>;
 
 const Login = () => {
     const navigate = useNavigate();
+    const [ulrParams] = useSearchParams();
 
     const { register, handleSubmit, formState: { errors },
     } = useForm<LoginFormSchema>({
         resolver: zodResolver(loginFormSchema)
     });
 
-    function loginFormFilter(data: LoginFormSchema) {
-        navigate(`/schedules?teste=${"data"}`);
+    async function loginFormFilter(data: LoginFormSchema) {
+        // fetch(`${API_URL}/user/login`, {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application/json"
+        //     },
+        //     credentials: "include",
+        //     body: JSON.stringify(data)
+        // })
+        //     .then((data) => data.json())
+        //     .then((data) => {
+        //         if (data.success) {
+        //             console.log("Usuário autenticado!");
+        //             navigate(`/schedules`);
+
+        //         } else {
+        //             console.log("Erro de login:", data.error);
+        //         }
+        //     })
+        //     .catch((err) => console.log(err));
     }
+
+    useEffect(() => {
+        if (ulrParams.get("registerMessage")) {
+
+            const timeout = setTimeout(() => {
+                navigate(window.location.pathname, { replace: true });
+            }, 3000);
+
+            return () => clearTimeout(timeout);
+        }
+    }, [ulrParams, navigate]);
+
     return (
         <div className={style.mainContainer}>
             <div className={style.containerLogo}>
@@ -34,11 +67,21 @@ const Login = () => {
                 </div>
             </div>
             <div className={style.contentContainer}>
+                {ulrParams.get("registerMessage") &&
+                    <p>Conta criada com sucesso!</p>
+                }
                 <form method="POST" onSubmit={handleSubmit(loginFormFilter)}>
                     <h2>Entre na sua conta</h2>
                     <p>Informe suas credenciais de acesso.</p>
-                    <input type="email" placeholder="" {...register("email")} required id="userEmail" name="userEmail" />
-                    <label htmlFor="userEmail"
+                    <input
+                        type="email"
+                        placeholder=""
+                        {...register("email")}
+                        required
+                        id="email"
+                    // name="userEmail" 
+                    />
+                    <label htmlFor="email"
                         className={`${style.labelForm} ${style.emailLabel}`}
                     >
                         E-mail
@@ -47,19 +90,27 @@ const Login = () => {
                     <p className={style.errorMessage}>
                         {errors.email && errors.email.message}
                     </p>
-                    <input type="password" placeholder="" {...register("userPassword")} required id="userPassword" name="userPassword" />
+                    <input
+                        type="password"
+                        placeholder=""
+                        {...register("password")}
+                        required
+                        id="password"
+                    // name="userPassword"
+                    />
 
-                    <label htmlFor="userPassword"
+                    <label htmlFor="password"
                         className={`${style.labelForm} ${style.passwordLabel}`}>
                         Senha
                     </label>
                     <p className={style.errorMessage}>
-                        {errors.userPassword && errors.userPassword.message}
+                        {errors.password && errors.password.message}
                     </p>
 
                     <input type="submit" value="Entrar" />
                     <Link to={"/register"}>Ainda não possui uma conta?</Link>
                 </form>
+
             </div>
         </div>
     )
--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -4,15 +4,16 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import style from "./Register.module.css";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import Logo from "../components/Logo";
+import { API_URL } from "../config";
 
 const registerFormSchema = z.object({
     username: z.string().min(5, "O usuário deve ter pelo menos 5 caracteres"),
-    userPassword: z.string().min(5, "A senha deve ter pelo menos 5 caracteres"),
-    userPasswordConfirm: z.string().min(5, "A confirmação deve ter pelo menos 5 caracteres"),
+    password: z.string().min(5, "A senha deve ter pelo menos 5 caracteres"),
+    confirmPassword: z.string().min(5, "A confirmação deve ter pelo menos 5 caracteres"),
     email: z.email({ message: "Informe um e-mail válido" }),
-}).refine((data) => data.userPassword === data.userPasswordConfirm, {
+}).refine((data) => data.password === data.confirmPassword, {
     message: "As senhas não coincidem",
-    path: ["userPasswordConfirm"],
+    path: ["confirmPassword"],
 });
 
 type RegisterFormSchema = z.infer<typeof registerFormSchema>;
@@ -26,7 +27,23 @@ const Register = () => {
     });
 
     function registerFormFilter(data: RegisterFormSchema) {
-        navigate("/login");
+        fetch(`${API_URL}/user/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: "include",
+            body: JSON.stringify(data)
+        })
+            .then((data) => data.json())
+            .then((data) => {
+                if (data.success) {
+                    navigate(`/login?registerMessage=true`);
+                } else {
+                    console.log("Erro de registro:", data.error);
+                }
+            })
+            .catch((err) => console.log(err));
     }
 
     return (
@@ -42,32 +59,59 @@ const Register = () => {
                 <form method="POST" onSubmit={handleSubmit(registerFormFilter)} >
                     <h2>Criar uma nova conta</h2>
                     <p>Preencha os campos abaixo para criar sua conta.</p>
-                    <input type="text" placeholder="" {...register("username")} required id="username" name="username" className={style.username}/>
+                    <input type="text"
+                        placeholder=""
+                        {...register("username")}
+                        required
+                        id="username"
+                        className={style.username}
+                    />
                     <label htmlFor="username" className={`${style.usernameLabel} ${style.labelForm}`}>Nome de usuário</label>
 
                     <p className={style.errorMessage}>
                         {errors.username && errors.username.message}
                     </p>
 
-                    <input type="email" placeholder="" {...register("email")} required id="userEmail" name="userEmail" className={style.email}/>
-                    <label htmlFor="userEmail" className={`${style.emailLabel} ${style.labelForm}`}>E-mail</label>
+                    <input
+                        type="email"
+                        placeholder=""
+                        {...register("email")}
+                        required
+                        id="email"
+                        className={style.email}
+                    />
+                    <label htmlFor="email" className={`${style.emailLabel} ${style.labelForm}`}>E-mail</label>
 
                     <p className={style.errorMessage}>
                         {errors.email && errors.email.message}
                     </p>
 
-                    <input type="password" placeholder="" {...register("userPassword")} required id="userPassword" name="userPassword" className={style.password}/>
-                    <label htmlFor="userPassword" className={`${style.passwordLabel} ${style.labelForm}`}>Senha</label>
+                    <input
+                        type="password"
+                        placeholder=""
+                        {...register("password")}
+                        required
+                        id="password"
+                        className={style.password}
+                    />
+                    <label htmlFor="password" className={`${style.passwordLabel} ${style.labelForm}`}>Senha</label>
 
                     <p className={style.errorMessage}>
-                        {errors.userPassword && errors.userPassword.message}
+                        {errors.password && errors.password.message}
                     </p>
 
-                    <input type="password" placeholder="" {...register("userPasswordConfirm")} required id="userConfirmPassword" name="userConfirmPassword" className={style.confirmPassword}/>
-                    <label htmlFor="userConfirmPassword" className={`${style.confirmPasswordLabel} ${style.labelForm}`}>Confirmar senha</label>
+                    <input
+                        type="password"
+                        placeholder=""
+                        {...register("confirmPassword")}
+                        required
+                        id="confirmPassword"
+                        className={style.confirmPassword}
+                    />
+                    <label htmlFor="confirmPassword" className={`${style.confirmPasswordLabel} ${style.labelForm}`}>Confirmar senha</label>
 
                     <p className={style.errorMessage}>
-                        {errors.userPasswordConfirm && errors.userPasswordConfirm.message}
+                        {errors.confirmPassword && errors.confirmPassword.message}
                     </p>
 
                     <input type="submit" value="Criar conta" />
